6d2ae33951bcf01e9968cc25b3a121c204faa4e4
Matt Johnson <mattajohnson26@gmail.com>
2024-08-24 12:45:39 -0600

    setting up routes

--- README.md
+++ README.md
@@ -7,20 +7,15 @@ An app that has a landing page and notes page where users can write and delete n
 ## Table of Contents
 - [Installation](#installation)
 - [Usage](#usage)
-- [Contributing](#contributing)
 - [License](#license)
 - [Tests](#tests)
 - [Questions](#questions)
 
 ## Installation
-Use NPM to install [Inquirer](https://www.npmjs.com/package/inquirer)
 Use NPM to install [Express]()
 
 ## Usage
 ![Screenshot]()
-
-## Contributing
-Follow this link: [Note Taker](github.com/MattAJ26/Note-Taker)
 
 ## License
 
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,19 +1,26 @@
 const router = require('express').Router();
 const path = require('path');
 const fs = require('fs');
-const uuid = require('uuid');
 
-router.get('/', (req, res) => {
+// --> ALL of these routes are prefixed with '/api'
+router.get('/notes', (req, res) => {
   readFromFile('./db.json').then((data) => res.json(JSON.parse(data)));
 });
 
 //post route needs logic
-router.post('/', (req, res) => {
-  console.log(req.body);
-
-  const newNote = { ...req.body, id: uuid() };
+router.post('/notes', (req, res) => {
+  console.log("Req Obj: ", req)
+  console.log("Incoming Data: ", req.body); // { title: "", content: ""}
+  // using a temp variable we can ADD/MODTIFY the Data as needed
+  let newNote = {
+    id: "5",
+    title: req.body.title,
+    text: req.body.text
+  }
 });
 
-router.delete();
+router.delete('/notes/:id', (req, res) => {
+   console.log("Req Params: ", req.params);  // { id: 6 }
+});
 
 module.exports = router;
