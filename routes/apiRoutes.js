7a9e71c259f0607cdb00098ca6acf37f6a4e93c3
Matt Johnson <mattajohnson26@gmail.com>
2024-08-24 19:28:37 -0600

    added logic to post route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
 const path = require('path');
 const fs = require('fs');
-
+const uuid = require('');
+ 
 // --> ALL of these routes are prefixed with '/api'
 router.get('/notes', (req, res) => {
   readFromFile('./db.json').then((data) => res.json(JSON.parse(data)));
@@ -9,18 +10,25 @@ router.get('/notes', (req, res) => {
 
 //post route needs logic
 router.post('/notes', (req, res) => {
-  console.log("Req Obj: ", req)
-  console.log("Incoming Data: ", req.body); // { title: "", content: ""}
+  console.log("Req Obj: ", req);
+  console.log("Incoming Data: ", req.body);
   // using a temp variable we can ADD/MODTIFY the Data as needed
+  const { title, text } = req.body;
+  if (title && text) {
   let newNote = {
-    id: "5",
-    title: req.body.title,
-    text: req.body.text
-  }
+    id: uuid(),
+    title,
+    text,
+  };
+  readAndAppend(newNote, './db.json');
+  res.json(`Note added successfully`);
+} else {
+  res.error('Error in adding note');
+}
 });
 
 router.delete('/notes/:id', (req, res) => {
-   console.log("Req Params: ", req.params);  // { id: 6 }
+  console.log("Req Params: ", req.params);
 });
 
 module.exports = router;
